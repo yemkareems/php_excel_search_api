a5f37b61369e6b1cbee12d161b44df6f83efce31
kareem <k.shareef@bambelo.com>
2022-06-07 12:48:30 +0530

    getting validation constraints moved to function

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -7,13 +7,13 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Service\SpreadSheet;
-use Symfony\Component\Validator\Validation;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class SearchController extends AbstractController
 {
 
-    public function searchAction(SpreadSheet $spreadSheetService, Request $request): JsonResponse
+    public function searchAction(SpreadSheet $spreadSheetService, Request $request, ValidatorInterface $validator): JsonResponse
     {
 
         $searchParams = [
@@ -23,15 +23,8 @@ class SearchController extends AbstractController
             'location' => $request->get('location'),
         ];
 
-        $validator = Validation::createValidator();
-        $constraint = new Assert\Collection(array(
-            // the keys correspond to the keys in the input array
-            'storage' => new Assert\Length(array('min' => 1, 'max' => 255)),
-            'ram' => new Assert\Choice(['2GB', '4GB', '8GB', '12GB', '16GB', '24GB', '32GB', '48GB', '64GB', '96GB'], NULL, TRUE),
-            'diskType' => new Assert\Choice(['SAS', 'SATA', 'SSD']),
-            'location' => new Assert\Choice(['AmsterdamAMS-01', 'DallasDAL-10', 'FrankfurtFRA-10', 'Hong KongHKG-10', 'San FranciscoSFO-12', 'SingaporeSIN-11', 'Washington D.C.WDC-01']),
-        ));
-        $violations = $validator->validate($searchParams, $constraint);
+        $constraints = $this->getValidationConstraints();
+        $violations = $validator->validate($searchParams, $constraints);
         if ($violations->count() > 0) {
             return new JsonResponse(["error" => (string)$violations], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
@@ -46,5 +39,16 @@ class SearchController extends AbstractController
         } catch (\Exception $e) {
             return new JsonResponse(['error' => 'Error reading the file'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
+    }
+
+    private function getValidationConstraints(): Assert\Collection
+    {
+        return new Assert\Collection(array(
+            'storage' => new Assert\Length(array('min' => 1, 'max' => 255)),
+            'ram' => new Assert\Choice(['2GB', '4GB', '8GB', '12GB', '16GB', '24GB', '32GB', '48GB', '64GB', '96GB'], null, true),
+            'diskType' => new Assert\Choice(['SAS', 'SATA', 'SSD']),
+            'location' => new Assert\Choice(['AmsterdamAMS-01', 'DallasDAL-10', 'FrankfurtFRA-10', 'Hong KongHKG-10', 'San FranciscoSFO-12', 'SingaporeSIN-11', 'Washington D.C.WDC-01']),
+        ));
+
     }
 }
