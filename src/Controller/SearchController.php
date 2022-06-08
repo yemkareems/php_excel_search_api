678f53f5b9167ec06b34bdadea1b92e8faeb4205
kareem <k.shareef@bambelo.com>
2022-06-08 09:35:58 +0530

    validator added separately to validate Input

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -7,13 +7,12 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Service\SpreadSheet;
-use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
+use App\Validator\InputValidator;
 
 class SearchController extends AbstractController
 {
 
-    public function searchAction(SpreadSheet $spreadSheetService, Request $request, ValidatorInterface $validator): JsonResponse
+    public function searchAction(SpreadSheet $spreadSheetService, Request $request, InputValidator $validator): JsonResponse
     {
 
         $searchParams = [
@@ -24,10 +23,10 @@ class SearchController extends AbstractController
             'location' => $request->get('location'),
         ];
 
-        $constraints = $this->getValidationConstraints();
-        $violations = $validator->validate($searchParams, $constraints);
-        if ($violations->count() > 0) {
-            return new JsonResponse(["error" => (string)$violations], Response::HTTP_INTERNAL_SERVER_ERROR);
+        $errorMessages = $validator->validate($searchParams);
+
+        if ($errorMessages) {
+            return new JsonResponse(["error" => $errorMessages], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
         $dataSource = getcwd().'/../'.$this->getParameter('dataSource').'/';
         try {
@@ -42,15 +41,4 @@ class SearchController extends AbstractController
         }
     }
 
-    private function getValidationConstraints(): Assert\Collection
-    {
-        return new Assert\Collection(array(
-            'storageFrom' => new Assert\Choice(['0GB', '250GB', '500GB', '1TB', '2TB', '3TB', '4TB', '8TB', '12TB', '24TB', '48TB', '72TB', '100TB']),
-            'storageTo' => new Assert\Choice(['0GB', '250GB', '500GB', '1TB', '2TB', '3TB', '4TB', '8TB', '12TB', '24TB', '48TB', '72TB', '100TB']),
-            'ram' => new Assert\Choice(['2GB', '4GB', '8GB', '12GB', '16GB', '24GB', '32GB', '48GB', '64GB', '96GB', '128GB'], null, true),
-            'diskType' => new Assert\Choice(['SAS', 'SATA', 'SSD']),
-            'location' => new Assert\Choice(['AmsterdamAMS-01', 'DallasDAL-10', 'FrankfurtFRA-10', 'Hong KongHKG-10', 'San FranciscoSFO-12', 'SingaporeSIN-11', 'Washington D.C.WDC-01']),
-        ));
-
-    }
 }
---
+++ src/Validator/InputValidator.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Validator;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Validation;
+
+class InputValidator
+{
+    public function validate(array $validateInput): array
+    {
+        $constraints = new Assert\Collection(array(
+            'storageFrom' => new Assert\Choice(['0GB', '250GB', '500GB', '1TB', '2TB', '3TB', '4TB', '8TB', '12TB', '24TB', '48TB', '72TB', '100TB']),
+            'storageTo' => new Assert\Choice(['0GB', '250GB', '500GB', '1TB', '2TB', '3TB', '4TB', '8TB', '12TB', '24TB', '48TB', '72TB', '100TB']),
+            'ram' => new Assert\Choice(['2GB', '4GB', '8GB', '12GB', '16GB', '24GB', '32GB', '48GB', '64GB', '96GB', '128GB'], null, true),
+            'diskType' => new Assert\Choice(['SAS', 'SATA', 'SSD']),
+            'location' => new Assert\Choice(['AmsterdamAMS-01', 'DallasDAL-10', 'FrankfurtFRA-10', 'Hong KongHKG-10', 'San FranciscoSFO-12', 'SingaporeSIN-11', 'Washington D.C.WDC-01']),
+        ));
+        $message = [];
+
+        $validator = Validation::createValidator();
+        $violations = $validator->validate($validateInput, $constraints);
+        if($violations->count() > 0){
+            foreach ($violations as $violation) {
+                $message[$violation->getPropertyPath()][] = $violation->getMessage();
+            }
+        }
+
+        return $message;
+    }
+
+}
