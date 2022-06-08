ba6ef48e74055f121409f13151a8d1fd73d9fcbb
kareem <k.shareef@bambelo.com>
2022-06-08 10:50:20 +0530

    storage Values set in a separate array for both from and to

--- src/Validator/InputValidator.php
+++ src/Validator/InputValidator.php
@@ -8,9 +8,10 @@ class InputValidator
 {
     public function validate(array $validateInput): array
     {
+        $storageValues = ['0GB', '250GB', '500GB', '1TB', '2TB', '3TB', '4TB', '8TB', '12TB', '24TB', '48TB', '72TB', '100TB'];
         $constraints = new Assert\Collection(array(
-            'storageFrom' => new Assert\Choice(['0GB', '250GB', '500GB', '1TB', '2TB', '3TB', '4TB', '8TB', '12TB', '24TB', '48TB', '72TB', '100TB']),
-            'storageTo' => new Assert\Choice(['0GB', '250GB', '500GB', '1TB', '2TB', '3TB', '4TB', '8TB', '12TB', '24TB', '48TB', '72TB', '100TB']),
+            'storageFrom' => new Assert\Choice($storageValues),
+            'storageTo' => new Assert\Choice($storageValues),
             'ram' => new Assert\Choice(['2GB', '4GB', '8GB', '12GB', '16GB', '24GB', '32GB', '48GB', '64GB', '96GB', '128GB'], null, true),
             'diskType' => new Assert\Choice(['SAS', 'SATA', 'SSD']),
             'location' => new Assert\Choice(['AmsterdamAMS-01', 'DallasDAL-10', 'FrankfurtFRA-10', 'Hong KongHKG-10', 'San FranciscoSFO-12', 'SingaporeSIN-11', 'Washington D.C.WDC-01']),
